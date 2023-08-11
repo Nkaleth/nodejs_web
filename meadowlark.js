6b37e4d19d084e18b0f559746acc246c6052657b
Nkaleth <nseguralu@gmail.com>
2023-08-10 23:07:44 -0500

    Moved 'fortune cookie' into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const fortune = require('./lib/fortune');
 
 const app = express();
-
-const fortunes = [
-  'Conquer your fears or they will conquer you.',
-  'Rivers need springs.',
-  "Do not fear what you don't know.",
-  'You will have a pleasant surprise.',
-  'Whenever possible, keep it simple.',
-];
 
 // configure Handlebars view engine
 app.engine('handlebars', engine());
@@ -23,21 +16,20 @@ app.use(express.static(`${__dirname}/public`));
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
 app.use((req, res) => {
   res.status(404);
-  res.render('404 - Not Found');
+  res.render('404');
 });
 
 // curstom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message);
   res.status(500);
-  res.render('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + 'press Ctrl-C to terminate.'));
